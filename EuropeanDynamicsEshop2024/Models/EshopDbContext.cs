46ec14a5c0b5ed1a4fca0fb5d3222bf3655f2246
Dimitris Iracleous <diracleous@athtech.gr>
2024-10-25 10:04:26 +0300

    model-migration

---
+++ EuropeanDynamicsEshop2024/Models/EshopDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuropeanDynamicsEshop2024.Models;
+
+public class EshopDbContext:DbContext
+{
+
+    public DbSet<Product> Products { get; set; } 
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "Data Source=(local);Initial Catalog=ed-eshop-2024; Integrated Security = True;TrustServerCertificate=True;";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+}
--- EuropeanDynamicsEshop2024/Models/Product.cs
+++ EuropeanDynamicsEshop2024/Models/Product.cs
@@ -1,20 +1,67 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EuropeanDynamicsEshop2024.Models;
 
+
+/// <summary>
+/// 
+///
+/// 
+/*
+   Product
+	Id, Name, Price, Category, Description, Color, Size, Weight, Stock
+Customer
+    Id, FistName, LastName, [Email], RegistrationDate, Active, [Address], Password, [Orders]
+Order
+    Id, <Customer>, [Product], OrderDate, TotalAmount
+*/
+/// 
+/// </summary>
+
 public class Product
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; } = string.Empty;
     public Category Category { get; set; }
+    [Precision(8,2)]
     public decimal Price { get; set; }
     public int Quantity { get; set; } = 1;
+    [Precision(8, 2)]
     public decimal DiscountPerCentPetItem { get; set; }
-    public ProductSupplier? Supplier { get; set; }
+    [Precision(8, 2)]
     public decimal TotalBeforeTaxes => Price*(1 - DiscountPerCentPetItem)  * Quantity;
+    public string? Size { get; set; }
+    [Precision(8, 2)]
+    public decimal Weight { get; set; }
+    public int Stock {  get; set; }
+    public List<Order> Orders { get; set; } = [];
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FistName { get; set; }= string.Empty;
+    public string  LastName { get; set; }= string.Empty;
+    public string Email { get; set; }= string.Empty; 
+    public DateTime RegistrationDate { get; set; }
+    public bool    Active { get; set; }
+    public List<Order> Orders { get; set; } = [];
+    
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public Customer? Customer { get; set; }
+    public List<Product> Products { get; set; } = [];
 }
