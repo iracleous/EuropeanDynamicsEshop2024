b26becda8c208b3a95720bceb910e46aaf2c51fd
Dimitris Iracleous <diracleous@athtech.gr>
2024-10-31 10:31:35 +0200

    controller

--- EshopApp/Controllers/EshopController.cs
+++ EshopApp/Controllers/EshopController.cs
@@ -1,4 +1,5 @@
 ﻿using EuropeanDynamicsEshop2024.Models;
+using EuropeanDynamicsEshop2024.Responses;
 using EuropeanDynamicsEshop2024.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,5 +22,29 @@ namespace EshopApi.Controllers
         {
             return _customerService.ReadCustomers();
         }
+        [HttpGet("customer/{id}")]
+        public ResponseApi<Customer> GetCustomer([FromRoute] int id)
+        {
+            return _customerService.ReadCustomer(id);
+        }
+
+        [HttpPost("customer")]
+        public ResponseApi<Customer> CreateCustomer([FromBody] Customer customer)
+        {
+            return _customerService.CreateCustomer(customer);
+        }
+
+        [HttpPut("customer")]
+        public ResponseApi<Customer> UpdateCustomer([FromBody] Customer customer)
+        {
+            return _customerService.UpdateCustomer(customer);
+        }
+
+        [HttpDelete("customer/{id}")]
+        public bool DeleteCustomer([FromRoute]  int id)
+        {
+            return _customerService.DeleteCustomer(id);
+        }
+
     }
 }
